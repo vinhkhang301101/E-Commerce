675be2aec74bf2ea4c445de61627e08f0f4b107f
Tris <minhtri240301@gmail.com>
2023-04-19 09:24:42 +0700

    update pages

--- src/config/path.js
+++ src/config/path.js
@@ -1,4 +1,8 @@
 export const PATH = {
     product: '/product',
     shopping_cart: '/shopping-cart',
+    auth: '/auth',
+    accountAddressEdit: '/account-address-edit',
+    accountAdress: '/account-address',
+    accountOrder: 'account-order',
 }
--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -4,6 +4,10 @@ import { Page404 } from "@/pages/404";
 import { PATH } from "@/config/path";
 import { Product } from "@/pages/product";
 import { ShoppingCart } from "@/pages/shopping-cart";
+import { Auth } from "@/pages/auth";
+import { AccountAddressEdit } from "@/pages/accountAddressEdit";
+import { AccountAdress } from "@/pages/accountAdress";
+import { AccountOrder } from "@/pages/accountOrder";
 
 export const routers = [
   {
@@ -22,6 +26,22 @@ export const routers = [
       {
         path: PATH.shopping_cart,
         element: <ShoppingCart />,
+      },
+      {
+        path: PATH.auth,
+        element: <Auth />,
+      },
+      {
+        path: PATH.accountAddressEdit,
+        element: <AccountAddressEdit />,
+      },
+      {
+        path: PATH.accountAdress,
+        element: <AccountAdress />,
+      },
+      {
+        path: PATH.accountOrder,
+        element: <AccountOrder />,
       },
 
       {
