6b4d1d97da85126cf5b10002f2a4ffad2efb6ba8
Tris <minhtri240301@gmail.com>
2023-05-14 21:25:43 +0700

    update profile

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -64,7 +64,7 @@ export const Profile = () => {
         userService.changePassword(...params);
       },
     });
-    // reup github
+    // reup github again
   const onSubmit = async () => {
     try {
       if (userForm.validate()) {
